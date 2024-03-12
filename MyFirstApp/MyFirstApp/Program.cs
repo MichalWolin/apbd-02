1c18bdd8f3937bb1f28fca63d9be9c9918af9da9
Michał Woliński <s29239@pjwstk.edu.pl>
2024-03-12 13:18:58 +0100

    modyfikacja 3

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -7,5 +7,6 @@ public class Class
         Console.WriteLine("Hello APBD");
         Console.WriteLine("Modyfikacja 1");
         Console.WriteLine("Modyfikacja 2");
+        Console.WriteLine("Modyfikacja 3");
     }
 }
