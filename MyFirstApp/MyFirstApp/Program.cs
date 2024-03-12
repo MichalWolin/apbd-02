c9e1611e718ed795ba803fe8d78fa1b58e51b986
Michał Woliński <s29239@pjwstk.edu.pl>
2024-03-12 13:11:27 +0100

    initial project

---
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Class
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello APBD");
+    }
+}
